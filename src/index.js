ba89c3a4dca6e07df1d61f14e5aca404fa7bb434
JonasLennie <contact@jlennie.uk>
2022-08-04 14:43:32 +0100

    Firebase setup

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCSlIeJJIXAC-FkYiDK79DCHqccqSpcdG4",
-    authDomain: "bedtransfer-e9489.firebaseapp.com",
-    projectId: "bedtransfer-e9489",
-    storageBucket: "bedtransfer-e9489.appspot.com",
-    messagingSenderId: "358809080094",
-    appId: "1:358809080094:web:abd925ad2a62439aa7b90f"
-};
-
-const app = initializeApp(firebaseConfig);
+// import { initializeApp } from "firebase/app";
+//
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCSlIeJJIXAC-FkYiDK79DCHqccqSpcdG4",
+//     authDomain: "bedtransfer-e9489.firebaseapp.com",
+//     projectId: "bedtransfer-e9489",
+//     storageBucket: "bedtransfer-e9489.appspot.com",
+//     messagingSenderId: "358809080094",
+//     appId: "1:358809080094:web:abd925ad2a62439aa7b90f"
+// };
+//
+// const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
