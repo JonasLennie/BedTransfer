7b1f3a0171610010c394e01da24219b9a1dc8e55
JonasLennie <contact@jlennie.uk>
2022-08-04 12:23:45 +0100

    Firebase setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCSlIeJJIXAC-FkYiDK79DCHqccqSpcdG4",
+    authDomain: "bedtransfer-e9489.firebaseapp.com",
+    projectId: "bedtransfer-e9489",
+    storageBucket: "bedtransfer-e9489.appspot.com",
+    messagingSenderId: "358809080094",
+    appId: "1:358809080094:web:abd925ad2a62439aa7b90f"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +23,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
